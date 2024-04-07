dd9ae683397941b97c46038273aadda12503ded3
Tobias Zorzetto <t166214@dac.unicamp.br>
2024-04-07 00:15:38 -0300

    server commented

--- P1/server.c
+++ P1/server.c
@@ -1,7 +1,3 @@
-/*
-** listener.c -- a datagram sockets "server" demo
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -20,6 +16,9 @@
 
 
 cJSON* read_json_string(char* buffer){
+    /*
+        reads a string from a buffer and returns it as a cJSON json object
+    */
     cJSON *json = cJSON_Parse(buffer); 
     if (json == NULL) { 
         const char *error_ptr = cJSON_GetErrorPtr(); 
@@ -33,7 +32,10 @@ cJSON* read_json_string(char* buffer){
 }
 
 cJSON* read_json() {
-    FILE *fp = fopen("example.json", "r"); 
+    /*
+        reads songs_data.json file and returns its content as a cJSON json object
+    */
+    FILE *fp = fopen("songs_data.json", "r"); 
     if (fp == NULL) { 
         printf("Error: Unable to open the file.\n"); 
         return NULL; 
@@ -47,23 +49,27 @@ cJSON* read_json() {
 }
 
 void write_json(cJSON* json) { 
-
+    /*
+        receives a cJSON json object, turns it into a string and write it to songs_data.json file
+    */
    char *json_str = cJSON_Print(json); 
   
    // write the JSON string to a file 
-   FILE *fp = fopen("example.json", "w"); 
+   FILE *fp = fopen("songs_data.json", "w"); 
    if (fp == NULL) { 
        printf("Error: Unable to open the file.\n"); 
        return; 
    } 
-   printf("%s\n", json_str); 
    fputs(json_str, fp); 
    fclose(fp); 
 }
     
 char* read_request(char *request,  cJSON *json){
-    printf("%s\n", request);
-    printf("\n");
+    /*
+        reads received request and returns a response buffer.
+        the buffers content depends on what operation was requested.
+    */
+    printf("request: %s\n", request);
     cJSON *elem;
     cJSON *name;
     cJSON *name_fetched;
@@ -77,13 +83,15 @@ char* read_request(char *request,  cJSON *json){
     switch (request[0])
     {
     case '1':
+        //1. Register a new song
+        // returns 0/1 if failed/succeded
         cJSON* song = read_json_string(2+request);
         for (int i = 0; i < n; i++) {
             elem = cJSON_GetArrayItem(json, i);
             id_fetched = cJSON_GetObjectItem(elem, "ID");
             if(strcmp(id_fetched->valuestring, cJSON_GetObjectItem(song, "ID")->valuestring) == 0){
-                response = "0";
-                break;
+                response = "0"; //cannot register a song with an existing ID
+                return response;
             }
         }
         cJSON_AddItemToArray(json, song);
@@ -91,6 +99,8 @@ char* read_request(char *request,  cJSON *json){
         response = "1";
         break;
     case '2':
+        //2. Delete a song
+        // returns 0/1 if failed/succeded
         char id[MAXBUFLEN];
         strcpy(id, 2+request);
         for (int i = 0; i < n; i++) {
@@ -103,29 +113,24 @@ char* read_request(char *request,  cJSON *json){
                 return response;
             }
         }
-        response = "0";
+        response = "0"; //ID doesnt exist
         break;
     case '3':
-        printf("%s\n", request);
-        printf("\n");
+        //3. List all songs from a year
+        // returns [song_object, ...] being song_object each of the songs from the year requested 
         strcpy(year, 2+request);
-        printf("%s", year);
-        printf("\n");
         for (int i = 0; i < n; i++) {
             elem = cJSON_GetArrayItem(json, i);
-            printf("kkkkkkkkkkkkkkk\n");
             name = cJSON_GetObjectItem(elem, "Release Date");
-            printf("hahahah\n");
-            printf("%s", name->valuestring);
-            printf("\n");
             if(strcmp(name->valuestring, year) == 0){
                 cJSON_AddItemToArray(songs, elem);
             }
         }
-        printf("wow");
         response = cJSON_Print(songs);
         break;
     case '4':
+        //4. List all songs of a certain language from a certain year
+        // returns [song_object, ...] being song_object each of the songs from the year and language requested  
         char language[MAXBUFLEN];
         cJSON *language_fetched;
         strcpy(language, 7+request);
@@ -140,7 +145,9 @@ char* read_request(char *request,  cJSON *json){
         }
         response = cJSON_Print(songs);
         break;
-    case '5':        
+    case '5':
+        //5. List all songs of a genre
+        // returns [song_object, ...] being song_object each of the songs from thegenre requested    
         cJSON *genre_fetched;
         strcpy(genre, 2+request);
         for (int i = 0; i < n; i++) {
@@ -153,18 +160,22 @@ char* read_request(char *request,  cJSON *json){
         response = cJSON_Print(songs);
         break;
     case '6':
+        //6. List all info from a certain song
+        // returns [json object]/[] if the ID exists/does not exist
         strcpy(literal_name, 2+request);
         for (int i = 0; i < n; i++) {
             elem = cJSON_GetArrayItem(json, i);
-            name_fetched = cJSON_GetObjectItem(elem, "Title");
+            name_fetched = cJSON_GetObjectItem(elem, "ID");
             if(strcmp(name_fetched->valuestring, literal_name) == 0){
-                response = cJSON_Print(elem);
-                return response;
+                cJSON_AddItemToArray(songs, elem);
+                break;
             }
         }
-        response = "{}";
+        response = cJSON_Print(songs);
         break;
     case '7':
+        //7. List all the information from all songs
+        // returns [song_object, ...] being song_object each of the songs from database 
         response = cJSON_Print(json);
         break;
     default:
@@ -174,8 +185,10 @@ char* read_request(char *request,  cJSON *json){
     return response;
 }
 
-// get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
+    /*
+        get sockaddr
+    */
 {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
@@ -206,33 +219,34 @@ int main(void)
         return 1;
     }
 
-    // loop through all the results and bind to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("listener: socket");
-            continue;
-        }
+    // loop through all the results and bind to the first possible
+    p = servinfo;
 
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("listener: bind");
-            continue;
-        }
-
-        break;
+    //define socket
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+            p->ai_protocol)) == -1) {
+        perror("socket");
     }
 
+    // bind socket
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+        close(sockfd);
+        perror("bind");
+    }
+
+
+    // check if socket exists
     if (p == NULL) {
-        fprintf(stderr, "listener: failed to bind socket\n");
+        fprintf(stderr, "Failed to bind socket\n");
         return 2;
     }
 
     freeaddrinfo(servinfo);
 
-    printf("listener: waiting to recvfrom...\n");
     char* response;
+    // loop infinetly, everytime that receives somthing in the socket, fo some operation
     while(1){
+        printf("waiting to recvfrom...\n");
         addr_len = sizeof their_addr;
         if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
             (struct sockaddr *)&their_addr, &addr_len)) == -1) {
@@ -240,14 +254,15 @@ int main(void)
             exit(1);
         }
 
-        printf("listener: got packet from %s\n",
+        printf("got packet from %s\n",
             inet_ntop(their_addr.ss_family,
                 get_in_addr((struct sockaddr *)&their_addr),
                 s, sizeof s));
-        printf("listener: packet is %d bytes long\n", numbytes);
+        printf("packet is %d bytes long\n", numbytes);
         buf[numbytes] = '\0';
 
-        response =  read_request(buf, json);
+        response =  read_request(buf, json);// act!
+        // respond properly to the request:
         if ((numbytes = sendto(sockfd, response, strlen(response), 0,
              (struct sockaddr *)&their_addr, addr_len)) == -1) {
             perror("talker: sendto");
